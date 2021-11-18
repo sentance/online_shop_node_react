9e901e99b63c19c92a1337252a0a3b17061920fa
sentance <sentance22@gmail.com>
2021-11-18 11:29:29 +0200

    added form to registration and login

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,34 +1,32 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import {SHOP_ROUTE} from "../utils/constants";
+import {observer} from "mobx-react-lite";
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context)
     return (
 
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="secondary" expand="lg">
                 <Container>
-                    <NavLink to={SHOP_ROUTE}>Ehehhe</NavLink>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                    <NavLink to={SHOP_ROUTE} style={{color: "whitesmoke"}}>Online Shop</NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" style={{color: 'black'}}/>
+                    {user.isAuth ?
+                        <Nav className="ml-auto">
+                            <Button variant="outline-light">Admin</Button>
+                            <Button style={{marginLeft: 20}} variant="outline-light" onClick={()=>user.setIsAuth(!user.isAuth)}> Logout</Button>
                         </Nav>
-                    </Navbar.Collapse>
+                        :
+                        <Nav className="ml-auto">
+                            <Button variant="outline-light"  onClick={()=>user.setIsAuth(!user.isAuth)}> Login</Button>
+                        </Nav>
+                    }
                 </Container>
             </Navbar>
 
     );
-};
+});
 
 export default NavBar;
--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/constants";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
     return (
-        <div>
-            <h1>Login page</h1>
-        </div>
+       <Container className="d-flex justify-content-center align-items-center"
+        style={{height: window.innerHeight - 54}}
+       >
+           <Card style={{width: 600}} className="p-5">
+               <h2 className="m-auto">{isLogin ?  "Authorization" : "Registration"}</h2>
+               <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="enter your email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="enter your password..."
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {!isLogin ?
+                            <div>
+                                <NavLink to={LOGIN_ROUTE}>Login</NavLink>
+                            </div>
+                            :
+                            <div>
+                                <NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
+                            </div>
+                        }
+                    </Row>
+                   <Button
+                       className="mt-3 align-self-end"
+                       variant="outline-success"
+                   >
+                       {!isLogin ? "Register" : "Login"}
+                   </Button>
+               </Form>
+           </Card>
+
+       </Container>
     );
 };
 
